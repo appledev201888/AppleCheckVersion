3a3bfa7d2f1bbc097cc19ce739be3449f1d00c88
chenweibin <weibinchen@uutaka.com>
2019-01-21 11:55:15 +0800

    app 版本升级

---
+++ Classes/AppStoreInfoModel.h
@@ -0,0 +1,58 @@
+//
+//  AppStoreInfoModel.h
+//  CPMerchant
+//
+//  Created by weibin on 2017/4/25.
+//  Copyright © 2017年 cwb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppStoreInfoModel : NSObject
+
+/**
+ *  版本号
+ */
+@property(nonatomic,copy) NSString * version;
+
+/**
+ *  更新日志
+ */
+@property(nonatomic,copy)NSString *releaseNotes;
+
+/**
+ *  更新时间
+ */
+@property(nonatomic,copy)NSString *currentVersionReleaseDate;
+
+/**
+ *  APPId
+ */
+@property(nonatomic,copy)NSString *trackId;
+
+/**
+ *  bundleId
+ */
+@property(nonatomic,copy)NSString *bundleId;
+
+/**
+ *  AppStore地址
+ */
+@property(nonatomic,copy)NSString *trackViewUrl;
+
+/**
+ *  开发商
+ */
+@property(nonatomic,copy)NSString *sellerName;
+
+/**
+ *  文件大小
+ */
+@property(nonatomic,copy)NSString *fileSizeBytes;
+
+/**
+ *  展示图
+ */
+@property(nonatomic,strong)NSArray *screenshotUrls;
+
+@end
---
+++ Classes/AppStoreInfoModel.m
@@ -0,0 +1,15 @@
+//
+//  AppStoreInfoModel.m
+//  CPMerchant
+//
+//  Created by weibin on 2017/4/25.
+//  Copyright © 2017年 cwb. All rights reserved.
+//
+
+#import "AppStoreInfoModel.h"
+
+@implementation AppStoreInfoModel
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{}
+
+@end
